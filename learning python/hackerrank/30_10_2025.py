57cf3b70590475831b28f6f5a24071020fc5a182
Nguyen Tien Dang <nguyentiendang0106@gmail.com>
2025-10-30 11:32:22 +0700

    start hackerrank python

---
+++ learning python/hackerrank/30_10_2025.py
@@ -0,0 +1,7 @@
+"1. hello world"
+"2. https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true"
+"3. https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true"
+"4. https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true"
+"5. https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true"
+"6. https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true"
+"7. https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true"
--- learning python/test.py
+++ learning python/test.py
@@ -1,10 +1,17 @@
-import calendar
+# import calendar
 
-yy = 2025
-mm = 10
+# yy = 2025
+# mm = 10
 
-print(calendar.month(yy, mm))
+# print(calendar.month(yy, mm))
 
 # x = [0] * 3
 # x[0] += 1
 # print(x) # [1, 0, 0]
+
+new_list = [2, 4, 5,3, 7, 9, 14, 57,12, 10]
+
+new_list.sort()
+
+for i in range(len(new_list)):
+    print(new_list[i])
